7ef43014020d6f15335f006f241f7798394b7901
teekaytech <taofeek.olalere@feghas.com>
2021-05-29 09:02:16 +0100

    Add react router dom

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from '../Login';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
     <main className="App">
-      <Login />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/dashboard" component={Dashboard} />
+        </Switch>
+      </BrowserRouter>
     </main>
   );
 }
