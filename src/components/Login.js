0f9acf2ac7ecbf94b12b5bb9bdf0c3efa1eb85ac
teekaytech <taofeek.olalere@feghas.com>
2021-05-29 10:09:09 +0100

    Add dashboard component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,84 +1,105 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import LoginImg from '../assets/images/login.jpg';
 import Logo from '../assets/images/logo.svg';
 import '../assets/stylesheets/login.scss';
 
-const Login = () => (
-  <section className="d-flex align-items-center min-vh-100 py-3 py-md-0">
-    <div className="container">
-      <div className="card login-card">
-        <div className="row no-gutters">
-          <div className="col-md-5">
-            <img
-              src={LoginImg}
-              alt="login"
-              className="login-card-img"
-            />
-          </div>
-          <div className="col-md-7">
-            <div className="card-body">
-              <div className="brand-wrapper">
-                <img
-                  src={Logo}
-                  alt="logo"
-                  className="logo"
-                />
-              </div>
-              <p className="login-card-description">Sign into your account</p>
-              <form>
-                <div className="form-group">
-                  <label htmlFor="email" className="sr-only">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="form-control"
-                    placeholder="Email address"
-                  />
+const Login = () => {
+  const history = useHistory();
+  const [info, setInfo] = useState(false);
+  const [loginDetails, setLoginDetails] = useState({
+    username: '',
+    userType: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLoginDetails({ ...loginDetails, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { username, userType } = loginDetails;
+    if (userType.length > 0 && username.length > '') {
+      history.push({
+        pathname: '/dashboard',
+        state: { userData: loginDetails },
+      });
+    } else setInfo(true);
+  };
+
+  return (
+    <section className="d-flex align-items-center min-vh-100 py-3 py-md-0">
+      <div className="container">
+        <div className="card login-card">
+          <div className="row no-gutters">
+            <div className="col-md-5">
+              <img src={LoginImg} alt="login" className="login-card-img" />
+            </div>
+            <div className="col-md-7">
+              <div className="card-body">
+                <div className="brand-wrapper">
+                  <img src={Logo} alt="logo" className="logo" />
                 </div>
-                <div className="form-group mb-4">
-                  <label htmlFor="password" className="sr-only">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    className="form-control"
-                    placeholder="***********"
-                    suggested="password"
-                  />
-                </div>
-                <input
-                  name="login"
-                  id="login"
-                  className="btn btn-block login-btn mb-4"
-                  type="button"
-                  value="Login"
-                />
-              </form>
-              <a href="#!" className="forgot-password-link">
-                Forgot password?
-              </a>
-              <p className="login-card-footer-text">
-                {"Don't have an account? "}
-                <a href="#!" className="text-reset">
-                  Register here
+                <p className="login-card-description">Sign into your account</p>
+                <p className="text-danger">{ info && 'All fields are compulsory'}</p>
+                <form onSubmit={handleSubmit}>
+                  <div className="form-group">
+                    <label htmlFor="username" className="sr-only">
+                      Username
+                    </label>
+                    <input
+                      type="text"
+                      name="username"
+                      id="username"
+                      className="form-control"
+                      placeholder="Username"
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="form-group mb-4">
+                    <label htmlFor="userType" className="sr-only">
+                      User type:
+                    </label>
+                    <select
+                      name="userType"
+                      id="userType"
+                      className="form-control"
+                      onChange={handleChange}
+                    >
+                      <option value="">select...</option>
+                      <option value="admin">Admin</option>
+                      <option value="others">Others</option>
+                    </select>
+                  </div>
+                  <button
+                    className="btn btn-block login-btn mb-4"
+                    type="submit"
+                  >
+                    Login
+                  </button>
+                </form>
+                <a href="#!" className="forgot-password-link">
+                  Forgot password?
                 </a>
-              </p>
-              <nav className="login-card-footer-nav">
-                <a href="#!">Terms of use.</a>
-                <a href="#!">Privacy policy</a>
-              </nav>
+                <p className="login-card-footer-text">
+                  {"Don't have an account? "}
+                  <a href="#!" className="text-reset">
+                    Register here
+                  </a>
+                </p>
+                <nav className="login-card-footer-nav">
+                  <a href="#!">Terms of use.</a>
+                  <a href="#!">Privacy policy</a>
+                </nav>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Login;
--- src/components/containers/Dashboard.js
+++ src/components/containers/Dashboard.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 function Dashboard() {
+  const location = useLocation();
   return (
     <div>
       Hello Dashboard
+      {location.state.userData.username}
+      {location.state.userData.userType}
     </div>
   );
 }
